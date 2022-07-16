cf9dd99a4dff5e3e0cba41c1adc9b4832d53cb86
ericz02 <ezheng0720@gmail.com>
2022-07-16 00:00:53 -0400

    operation fixes

--- script.js
+++ script.js
@@ -16,11 +16,21 @@ class Calculator {
     }
 
     appendNumber(number) {
-        this.currentOperand = number
+        //if we type in . or if we already have . then return it
+        if (number === '.' && this.currentOperand.includes('.')) return 
+        this.currentOperand = this.currentOperand.toString() +  number.toString() 
     }
 
     chooseOperation(operation) {
-         
+        //if current operation is empty then return
+        if (this.currentOperand === '') return
+        //if previous operand is not equal to empty string
+        if (this.previousOperand !== '') {
+            this.compute()
+        }
+        this.operation = operation
+        this.previousOperand = this.currentOperand
+        this.currentOperand = ''
     }
 
     compute() {
@@ -29,6 +39,7 @@ class Calculator {
 
     updateDisplay() {
         this.currentOperandTextElement.innerText = this.currentOperand
+        this.previousOperandTextElement.innerText = this.previousOperand
     }
 }
 
@@ -42,9 +53,17 @@ const previousOperandTextElement = document.querySelector('[data-previous-operan
 const currentOperandTextElement = document.querySelector('[data-current-operand]')
 
 const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+
 numberButtons.forEach(button => {
     button.addEventListener('click', () => {
         calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+})
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText)
         calculator.updateDisplay()
     })
 })
